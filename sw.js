a96af229abe10f213ac91ada8a0ba3374f7ca666
cxyzzz <2283361094@qq.com>
2018-09-08 11:55:51 +0800

    Site updated: 2018-09-08 11:55:48

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/5799c0b3.html');
+  precacheUrls.push('/posts/9e7b6f05.html');
 
-  precacheUrls.push('/posts/9e47c568.html');
+  precacheUrls.push('/posts/3ba37e62.html');
 
-  precacheUrls.push('/posts/f5125d59.html');
+  precacheUrls.push('/posts/2443be7d.html');
 
-  precacheUrls.push('/posts/46992fc.html');
+  precacheUrls.push('/posts/f05bc4bf.html');
 
-  precacheUrls.push('/posts/ab85ea5f.html');
+  precacheUrls.push('/posts/d5ad1f4e.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
