fa5ae9abe85073032825fe1a8ddb4f459fea7380
cxyzzz <2283361094@qq.com>
2020-07-08 18:04:48 +0800

    Travis CI Auto Builder 2020-07-08 18:04:48 Build 252 With 21bc0d264a216af7335adfd4d3821535ea135bd8 Commit

--- js/duoshuoshuo.js
+++ js/duoshuoshuo.js
@@ -1,17 +1,1 @@
-<!--崩溃欺骗-->
-var OriginTitile = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/favicon-16x16-next.png");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/images/favicon-16x16.png");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitile;
-         }, 2000);
-     }
- });
+"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/images/favicon-16x16-next.png"),document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/images/favicon-16x16.png"),document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
--- js/sakura.js
+++ js/sakura.js
@@ -1,15 +1,1 @@
-    function sakuraInit() {
-        $(document).snowfall('clear');
-        if (document.body.clientWidth > 600) {
-            $(document).snowfall({image:"/images/sakura/1.png", flakeCount:7, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/1.png", flakeCount:7, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/2.png", flakeCount:7, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/4.png", flakeCount:7, minSpeed:1, minSize:8, maxSize:15,});
-        } else {
-            $(document).snowfall({image:"/images/sakura/1.png", flakeCount:3, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/1.png", flakeCount:3, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/2.png", flakeCount:3, minSpeed:1, minSize:8, maxSize:15,});
-            $(document).snowfall({image:"/images/sakura/4.png", flakeCount:3, minSpeed:1, minSize:8, maxSize:15,});
-        }
-    }
-    window.onload = sakuraInit();
+"use strict";function sakuraInit(){$(document).snowfall("clear"),600<document.body.clientWidth?($(document).snowfall({image:"/images/sakura/1.png",flakeCount:7,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/1.png",flakeCount:7,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/2.png",flakeCount:7,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/4.png",flakeCount:7,minSpeed:1,minSize:8,maxSize:15})):($(document).snowfall({image:"/images/sakura/1.png",flakeCount:3,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/1.png",flakeCount:3,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/2.png",flakeCount:3,minSpeed:1,minSize:8,maxSize:15}),$(document).snowfall({image:"/images/sakura/4.png",flakeCount:3,minSpeed:1,minSize:8,maxSize:15}))}window.onload=sakuraInit();
